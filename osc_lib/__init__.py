812e0741f584eaa4d12339a991d604a4a60b8b89
Alexander Tsamutali <atsamutali@mirantis.com>
2016-06-29 20:06:25 +0300

    Get VersionInfo of "osc-lib"
    Currently this package declares version of "python-openstackclient"
    instead of his own version.
    
    Change-Id: Ia6877bc3bb945cc59e92698943e8f9eb6fbf7df9

--- osc_lib/__init__.py
+++ osc_lib/__init__.py
@@ -15,7 +15,7 @@ __all__ = ['__version__']
 
 import pbr.version
 
-version_info = pbr.version.VersionInfo('python-openstackclient')
+version_info = pbr.version.VersionInfo('osc-lib')
 try:
     __version__ = version_info.version_string()
 except AttributeError:
