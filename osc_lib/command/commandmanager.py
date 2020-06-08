b6d1d539c875059fa64d6275011f2c6f5fe4b8e7
Monty Taylor <mordred@inaugust.com>
2020-06-07 20:54:44 -0500

    Remove commandmanager subclass
    We've moved this functionality into cliff, so remove it here. This
    will let us update cliff to optimize away the common case pkg_resources.
    
    Change-Id: I75922010bdb29dbea530b6527c8b71eb064fdb30

--- osc_lib/command/commandmanager.py
+++ osc_lib/command/commandmanager.py
@@ -13,46 +13,9 @@
 #   under the License.
 #
 
-"""Modify cliff.CommandManager"""
-
 import cliff.commandmanager
-import pkg_resources
 
 
 class CommandManager(cliff.commandmanager.CommandManager):
-    """Add additional functionality to cliff.CommandManager
-
-    Load additional command groups after initialization
-    Add _command_group() methods
-    """
-
-    def __init__(self, namespace, convert_underscores=True):
-        self.group_list = []
-        super(CommandManager, self).__init__(namespace, convert_underscores)
-
-    def load_commands(self, namespace):
-        self.group_list.append(namespace)
-        return super(CommandManager, self).load_commands(namespace)
-
-    def add_command_group(self, group=None):
-        """Adds another group of command entrypoints"""
-        if group:
-            self.load_commands(group)
-
-    def get_command_groups(self):
-        """Returns a list of the loaded command groups"""
-        return self.group_list
-
-    def get_command_names(self, group=None):
-        """Returns a list of commands loaded for the specified group"""
-        group_list = []
-        if group is not None:
-            for ep in pkg_resources.iter_entry_points(group):
-                cmd_name = (
-                    ep.name.replace('_', ' ')
-                    if self.convert_underscores
-                    else ep.name
-                )
-                group_list.append(cmd_name)
-            return group_list
-        return list(self.commands.keys())
+    """Noop subclass for transition purposes."""
+    pass
